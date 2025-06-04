6ffa17b15069365b7c92f8ee23c32af7b4fa1144
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-04 09:15:20 +0700

    refactor: move Container widget to Quiz

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,32 +5,28 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(color: const Color.fromARGB(255, 9, 40, 70)),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-              color: const Color.fromARGB(200, 255, 255, 255),
-            ),
-            const SizedBox(height: 30),
-            const Text(
-              'Learn Flutter in a Fun Way!',
-              style: TextStyle(fontSize: 24, color: Colors.white),
-            ),
-            const SizedBox(height: 20),
-            OutlinedButton.icon(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
-              icon: const Icon(Icons.play_arrow),
-              label: const Text('Start Quiz'),
-            ),
-          ],
-        ),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(200, 255, 255, 255),
+          ),
+          const SizedBox(height: 30),
+          const Text(
+            'Learn Flutter in a Fun Way!',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          const SizedBox(height: 20),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.play_arrow),
+            label: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
