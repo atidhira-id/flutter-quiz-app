78397c83f928f659106ad91eaf96a4e28a026575
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-04 09:16:12 +0700

    feat: add quiz class

---
+++ lib/quiz.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 9, 40, 70),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    );
+  }
+}
