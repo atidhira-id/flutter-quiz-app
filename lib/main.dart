3040bd0b1dc76d94207a78db9de2a35bad9dcc68
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-04 08:42:21 +0700

    refactor: seperate starting screen container widget into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-          padding: EdgeInsets.all(20),
-          decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 9, 40, 70),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset('assets/images/quiz-logo.png', width: 300),
-                Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Text(
-                    'Learn Flutter in a Fun Way!',
-                    style: TextStyle(fontSize: 24, color: Colors.white),
-                  ),
-                ),
-                OutlinedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'Start Quiz',
-                    style: TextStyle(fontSize: 18, color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+      home: Scaffold(body: StartScreen()),
     ),
   );
 }
