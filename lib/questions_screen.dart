b4f777b372531726d3babdac0fc2eb08e585e5b2
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-05 17:38:18 +0700

    feat: add function state to handle user selecting quiz answer

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(int questionNumber, String answer) onSelectAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -12,21 +15,43 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  int currentQuestionIndex = 0;
+
+  void answerQuestion(int questionNumber, String answer) {
+    widget.onSelectAnswer(questionNumber, answer);
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Text(currentQuestion.question, style: TextStyle(color: Colors.white)),
+          Text(
+            currentQuestion.question,
+            style: GoogleFonts.lato(
+              color: Colors.white,
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
           SizedBox(height: 20),
           ...currentQuestion.getShuffledAnswer().map((answer) {
             return Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                AnswerButton(answer, onTap: () {}),
+                AnswerButton(
+                  answer,
+                  onTap: () {
+                    answerQuestion(currentQuestionIndex, answer);
+                  },
+                ),
                 SizedBox(height: 8),
               ],
             );
