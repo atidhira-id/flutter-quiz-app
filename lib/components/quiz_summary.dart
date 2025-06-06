8bbf410ed8048580c7d2f3b1b538cf774e9f6cdd
Atidhira Habibillah <atidhiraid68@gmail.com>
2025-06-06 14:23:29 +0700

    refactor: seperate question number and summary column into its own Widget file

--- lib/components/quiz_summary.dart
+++ lib/components/quiz_summary.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/components/question_identifier.dart';
+import 'package:quiz_app/components/summary_item.dart';
 import 'package:quiz_app/models/question_summary.dart';
 
 class QuizSummary extends StatelessWidget {
@@ -18,49 +20,12 @@ class QuizSummary extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     SizedBox(width: 20),
-                    Container(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 12,
-                        vertical: 6,
-                      ),
-                      decoration: BoxDecoration(
-                        color:
-                            data.correctAnswer == data.userAnswer
-                                ? Colors.blue
-                                : Colors.red,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Text(
-                        (data.questionNumber + 1).toString(),
-                        style: TextStyle(color: Colors.black),
-                      ),
+                    QuestionIdentifier(
+                      data.questionNumber + 1,
+                      (data.correctAnswer == data.userAnswer),
                     ),
                     SizedBox(width: 20),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            data.question,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          SizedBox(height: 5),
-                          Text(
-                            "Your answer: ${data.userAnswer}",
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 215, 131, 131),
-                            ),
-                          ),
-                          Text(
-                            "Correct answer: ${data.correctAnswer}",
-                            style: TextStyle(
-                              color: const Color.fromARGB(255, 122, 183, 232),
-                            ),
-                          ),
-                          SizedBox(height: 15),
-                        ],
-                      ),
-                    ),
+                    Expanded(child: SummaryItem(data)),
                     SizedBox(width: 20),
                   ],
                 );
